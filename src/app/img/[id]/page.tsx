c0034aaf6587fcc1533dfd876108543b31804236
h.setiawan <hemasetiawan123@gmail.com>
2025-04-23 15:12:24 +0900

    fixed dialog full screen and made a components folder for full image view

--- src/app/img/[id]/page.tsx
+++ src/app/img/[id]/page.tsx
@@ -1,14 +1,13 @@
-import { getImage } from "~/server/queries";
+import FullPageImageView from "~/components/full-image-page";
 
-export default async function PhotoModal(props: { params: { id: string } }) {
-  // Do not destructure `params` in the parameter list!
-  const photoId = props.params.id;
+export default function PhotoModal({
+  params: { id: photoId },
+}: {
+  params: { id: string };
+}) {
   const idAsNumber = Number(photoId);
   if (Number.isNaN(idAsNumber)) throw new Error("Invalid photo id");
-  const image = await getImage(idAsNumber);
-  return (
-    <div>
-      <img src={image.url} className="w-full h-full object-cover" />
-    </div>
-  );
+  return <FullPageImageView id={idAsNumber} />;
+
+
 }
